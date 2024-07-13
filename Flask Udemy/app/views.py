847813bb3ba65f4d108dea73582b3b285c691aab
JosemirFarias <josemirfbrito@gmail.com>
2024-07-13 20:59:37 -0300

    Primeira atualização da aula.

--- Flask Udemy/app/views.py
+++ Flask Udemy/app/views.py
@@ -2,9 +2,20 @@
 from app import app
 
 # importando a os arquivos dentro da pasta template 
-from flask import render_template
+from flask import render_template, url_for # render_template é responsável por renderizar o arquivo html e o url_for é responsável por criar links das rotas
 
 # criando uma rota
 @app.route('/')
 def homepage():
-    return render_template('index.html') # renderizando o arquivo index.html
+    usuario = 'Josemir'
+    idade = 34
+
+    context = {
+        'usuario': usuario,
+        'idade': idade
+    }
+    return render_template('index.html', context=context) # renderizando o arquivo index.html
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html') # renderizando o arquivo sobre.html
