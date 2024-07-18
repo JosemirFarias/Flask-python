0cb4657df7f0548f47c16540f867685df542e011
JosemirFarias <josemirfbrito@gmail.com>
2024-07-17 21:36:50 -0300

    Estudando formulários

--- Flask Udemy/app/views.py
+++ Flask Udemy/app/views.py
@@ -4,8 +4,8 @@ from app import app
 # importando a os arquivos dentro da pasta template 
 from flask import render_template, url_for # render_template é responsável por renderizar o arquivo html e o url_for é responsável por criar links das rotas
 
-# criando uma rota
-@app.route('/')
+
+@app.route('/')  # criando uma rota para a página inicial
 def homepage():
     usuario = 'Josemir'
     idade = 34
@@ -16,6 +16,10 @@ def homepage():
     }
     return render_template('index.html', context=context) # renderizando o arquivo index.html
 
-@app.route('/sobre')
+@app.route('/sobre') # criando uma rota para a página sobre
 def sobre():
     return render_template('sobre.html') # renderizando o arquivo sobre.html
+
+@app.route('/contato')  # criando uma rota para a página de contato
+def contato():
+    return render_template('contato.html')
