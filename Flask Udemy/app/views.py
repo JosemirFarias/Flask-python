fe8c6e2eb3d52ff7f529e02ad7f5623900fc30c2
JosemirFarias <josemirfbrito@gmail.com>
2024-07-18 21:40:21 -0300

    Cont. formulários

--- Flask Udemy/app/views.py
+++ Flask Udemy/app/views.py
@@ -2,7 +2,7 @@
 from app import app
 
 # importando a os arquivos dentro da pasta template 
-from flask import render_template, url_for # render_template é responsável por renderizar o arquivo html e o url_for é responsável por criar links das rotas
+from flask import render_template, url_for, request  # render_template é responsável por renderizar o arquivo html / url_for é responsável por criar links das rotas / request é responsável por pegar os dados do formulário
 
 
 @app.route('/')  # criando uma rota para a página inicial
@@ -20,6 +20,12 @@ def homepage():
 def sobre():
     return render_template('sobre.html') # renderizando o arquivo sobre.html
 
-@app.route('/contato')  # criando uma rota para a página de contato
+@app.route('/contato', methods=['POST'])  # criando uma rota para a página de contato
 def contato():
-    return render_template('contato.html')
+    context = {}  # criando um dicionário vazio
+    if request.method == 'POST':  # verificando se o método é POST
+        pesquisa = request.form.get('pesquisa')  # pegando o valor do input com o name pesquisa
+        print(pesquisa)  # imprimindo o valor do input
+        context.update({'pesquisa': pesquisa})  # atualizando o dicionário com o valor do input
+   
+    return render_template('contato.html', context=context)  # renderizando o arquivo contato.html com o dicionário context
